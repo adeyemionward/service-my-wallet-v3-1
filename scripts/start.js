c98e107872653cdba902f3f981cce064f54954fb
adeyemionward <34043883+adeyemionward@users.noreply.github.com>
2021-02-07 15:21:00 +0100

    Update start.js

--- scripts/start.js
+++ scripts/start.js
@@ -7,7 +7,8 @@ try {
 } finally {
   var merchantAPI = require('../index.js');
   merchantAPI.start({
-    
+    port: process.env.PORT || 3000,
+    bind: process.env.BIND || '127.0.0.1'
   });
   if (process.env.RPC) {
     merchantAPI.startRPC({
